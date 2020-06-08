4987aa49a53358a07e1748862fefb5353922a121
ParkChan <pcremnant@naver.com>
2020-06-09 01:36:46 +0900

    add class { MainGUI }

---
+++ MainGUI.py
@@ -0,0 +1,15 @@
+from tkinter import *
+
+
+class MainGUI:
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("국가자격증")
+        self.window.geometry("1000x800")
+
+        
+        self.window.mainloop()
+
+
+
+
